160af60a032f5504dbfcf722b1d21c9dad647d60
michaeliyaomolere <mikoa04@gmail.com>
2019-03-21 18:09:12 -0400

    Update

--- Guess Game.py
+++ Guess Game.py
@@ -13,6 +13,18 @@ def game_error_checker():#This function helps to check the game for errors
     print("Would you like to continue " + name)
     print("type Y for yes or N for No")
     print("Please " + name + " Kindly input integers only, between 1 & 10")
-    play_game_func()
+    play_again_func()
+
+def play_again_func():# this function helps the player to play the game again
+
+    play_again = str(input())
+    play_again = play_again.upper()
+    if play_again == "Y":
+        game_engine()
+
+    elif play_again == "N":
+        print("*******Thanks For Playing," + name + " Goodbye******")
+
+        exit()
 
 
